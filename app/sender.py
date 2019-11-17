05cd714fbde2c89fd7de2ef4a0edfb11c1c0e33c
Rodrigo Martins de Freitas Alves <rodrigo.martins.50@hotmail.com>
2019-11-17 02:12:36 -0300

    Alterando algumas portas.

--- app/sender.py
+++ app/sender.py
@@ -42,4 +42,4 @@ class Sender(Bottle):
 
 if __name__ == '__main__':
     sender = Sender()
-    sender.run(host='0.0.0.0', port=8080, debug=True)
+    sender.run(host='0.0.0.0', port=8081, debug=True)
